75067d9aea863cc0440546c237acc768c2a015e7
Ian Jackson <iansjackson8@gmail.com>
2025-04-08 21:21:03 -0400

    tism thing

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
 import seaborn as sns
 
 from typing import Union, List
